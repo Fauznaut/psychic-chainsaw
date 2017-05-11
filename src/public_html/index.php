f22c08dc1dd2010200f779d8e53f626dcbe88d88
Aaron Belz <belzaaron@gmail.com>
2017-05-11 06:50:19 -0400

    :heavy_plus_sign: Added composer packages and a bower package
    ## Composer
    - twig/twig
    - IcyApril/CryptoLib
    - phpmailer/phpmailer
    
    
    ## Bower
    - Bootstrap
    
    
    ## Extra notes
    
    I will need to clean up the info in the Composer and Bower configs.

---
+++ src/public_html/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * +--------------------------+
+ * | PROCORD.ORG FRONT PORTAL |
+ * +--------------------------+
+ */
+
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
+
+/**
+* Auto load classes
+*/
+spl_autoload_register(function($class)
+{
+	// The parent directory
+	$root = dirname(__DIR__);
+	$file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+	if (is_readable($file))
+		require $file;
+});
+
+
+
+/**
+ * Composer
+ */
+require '../vendor/autoload.php';
+
+
+
+/**
+ * Twig
+ */
+Twig_Autoloader::register();
+
+
+\app\Config::init();
